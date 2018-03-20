0a447af58c099b614879e3caa7002c8e8d8d5b98
李守宪 <lishouxian@yidian-inc.com>
2018-03-20 14:54:58 +0800

    修改

--- library/src/main/java/com/sollian/library/Util.java
+++ library/src/main/java/com/sollian/library/Util.java
@@ -65,8 +65,6 @@ public class Util {
             window.getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
             );
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.TRANSPARENT);
